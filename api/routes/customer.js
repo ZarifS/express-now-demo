5be9d139ba2cc4dd64ced008ca70706eeaf5b521
Zarif Shahriar <zarif.shahriar@gmail.com>
2019-01-07 22:58:54 -0500

    Initial Commit

---
+++ api/routes/customer.js
@@ -0,0 +1,89 @@
+let CustomerModel = require('../models/customerModel')
+let express = require('express')
+let router = express.Router()
+
+//Create a new customer
+router.post('/customer', (req, res) => {
+    if (req.body) {
+        let model = new CustomerModel(req.body)
+        model
+            .save()
+            .then(doc => {
+                if (!doc || doc.length === 0) {
+                    return res
+                        .status(500)
+                        .send(doc)
+                }
+                res
+                    .status(201)
+                    .send(doc)
+            })
+            .catch(err => {
+                res
+                    .status(500)
+                    .json(err)
+            })
+    } else {
+        return res
+            .status(400)
+            .send('Request body is missing.')
+    }
+})
+
+//Get a customer by email
+router.get('/customer', (req, res) => {
+    if (!req.query.email) {
+        return res
+            .status(400)
+            .send('Missing URL parameter: email')
+    }
+    CustomerModel
+        .findOne({email: req.query.email})
+        .then(doc => {
+            res.json(doc)
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json(err)
+        })
+})
+
+//Update a customer by email
+router.put('/customer', (req, res) => {
+    if (!req.query.email) {
+        return res
+            .status(400)
+            .send('Missing URL parameter: email')
+    }
+    CustomerModel.findOneAndUpdate({
+        email: req.query.email
+    }, req.body, {new: true}).then(doc => {
+        res.json(doc)
+    }).catch(err => {
+        res
+            .status(500)
+            .json(err)
+    })
+})
+
+//Delete a customer by email
+router.delete('/customer', (req, res) => {
+    if (!req.query.email) {
+        return res
+            .status(400)
+            .send('Missing URL parameter: email')
+    }
+    CustomerModel
+        .findOneAndDelete({email: req.query.email})
+        .then(doc => {
+            res.json(doc)
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json(err)
+        })
+})
+
+module.exports = router
---
+++ api/server.js
@@ -0,0 +1,37 @@
+let express = require('express')
+let app = express()
+let path = require('path')
+let mongoose = require('mongoose')
+let bodyParser = require('body-parser')
+let customerRoute = require('./routes/customer')
+
+const serverName = 'ds151354.mlab.com:51354'
+const database = 'shopify-backend'
+const dbuser = 'zshahriar'
+const dbpassword = '6xdGArjh3ijf'
+mongoose.connect(`mongodb://${dbuser}:${dbpassword}@${serverName}/${database}`)
+
+app.use(bodyParser.json())
+app.use(customerRoute)
+app.use(express.static('public'))
+
+app.get('/', (res, req) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+//Handler for 404 - Resource not found
+app.use((req, res, next) => {
+    res
+        .status(404)
+        .send('We think you are lost!')
+})
+
+//Handler for 500
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.sendFile(path.join(__dirname, './public/500.html'))
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.info(`Server has started on ${port}`))

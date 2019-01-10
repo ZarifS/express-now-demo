6e1732e2714e49f8a529c168e7bd89f784578dd1
Zarif Shahriar <zarif.shahriar@gmail.com>
2019-01-10 18:25:56 -0500

    Small update

--- api/server.js
+++ api/server.js
@@ -8,7 +8,6 @@ let dbConnection = require('./dbConnection')
 dbConnection()
 app.use(bodyParser.json())
 app.use(customerRoute)
-
 app.get("/api", (req, res, next) => {
     //Home URL
     res.send('Use the /api method with the api documentation for REST Calls.')
@@ -19,14 +18,6 @@ app.use((req, res, next) => {
     res
         .status(404)
         .send('We think you are lost!')
-})
-
-//Handler for 500
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res
-        .status(404)
-        .sendFile(path.join(__dirname, '../public/500.html'))
 })
 
 const port = process.env.PORT || 3000
